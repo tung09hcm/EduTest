c67c29a67893b200918afa279e04c3e3135d1453
tung <tung.bui2004lk@hcmut.edu.vn>
2024-10-16 23:38:11 +0700

    add delete post from user feature

--- include/delete_post.php
+++ include/delete_post.php
@@ -1,5 +1,6 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
     // Lấy dữ liệu JSON từ request body
     $data = json_decode(file_get_contents('php://input'), true);
     $indexToDelete = $data['index'];  // Chỉ số bài viết cần xóa
@@ -11,6 +12,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         // Xóa bài viết theo chỉ số
         if (isset($posts[$indexToDelete])) {
+
+
+            if($_SESSION["username"] == $posts[$indexToDelete]['username'])
+            {
+                include 'database.php';
+                $sql = "DELETE FROM post WHERE ID = ?";
+                try {
+                    $stmt = $conn->prepare($sql); // Chuẩn bị câu lệnh
+                    if (!$stmt) {
+                        throw new Exception("Prepare failed: " . $conn->error);
+                    }
+        
+                    // Gán giá trị cho placeholder
+                    $stmt->bind_param("s", $posts[$indexToDelete]['id']); 
+        
+                    if (!$stmt->execute()) {
+                        throw new Exception("Execute failed: " . $stmt->error);
+                    }
+                    
+                    echo "Thêm nội dung vào post thành công."; // Thông báo thành công
+                } catch (Exception $e) {
+                    echo "Không thể thêm nội dung vào post: " . $e->getMessage(); // In ra lỗi
+                }
+            }
+
+
             array_splice($posts, $indexToDelete, 1);
             // Lưu lại danh sách bài viết sau khi xóa
             file_put_contents($file, json_encode($posts));
@@ -21,5 +48,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     } else {
         echo "Không tìm thấy file .json.";
     }
+
+
+
+
 }
 ?>
